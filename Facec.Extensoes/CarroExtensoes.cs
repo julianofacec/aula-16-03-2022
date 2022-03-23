18a274d89796452f0a3bc0b618c7ca295dcc243d
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-03-23 19:38:03 -0300

    exemplos estudados em aula

---
+++ Facec.Extensoes/CarroExtensoes.cs
@@ -0,0 +1,18 @@
+﻿using Facec.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facec.Extensoes
+{
+    public static class CarroExtensoes
+    {
+        public static string ObterVelocidadeFinal(this AbstractCarro carro)
+        {
+            return $"{carro.ObterMarca()} - {carro.ObterModelo()} :" +
+                $"{carro.ObterVelocidade()} Km/h \n";
+        }
+    }
+}
